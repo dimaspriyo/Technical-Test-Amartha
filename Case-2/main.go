54eb781363b3f3c9f5720d9fa89cdfe93e25cb0f
dimas <dimas.priyo@bluebirdgroup.com>
2025-05-16 16:42:45 +0700

    fix reconcile and return response

--- Case-2/main.go
+++ Case-2/main.go
@@ -23,6 +23,17 @@ type ReconcileRequest struct {
 	EndDate            string   `json:"end_date"`
 }
 
+type ReconcileResponse struct {
+	TotalTransactionsProceed   int                            `json:"total_transactions_proceed"`
+	TotalMatchedTransactions   int                            `json:"total_matched_transactions"`
+	TotalUnmatchedTransactions TotalUnmatchedTransactionsData `json:"total_unmatched_transactions"`
+}
+
+type TotalUnmatchedTransactionsData struct {
+	MissingTransactions   []Row            `json:"missing_transactions"`
+	MissingBankStatements map[string][]Row `json:"missing_bank_statements"`
+}
+
 type TransactionsReadResponse struct {
 	Rows                 [][]string
 	MapRows              map[int]bool
@@ -37,18 +48,19 @@ type ChannelResponse struct {
 	amount int
 }
 
-type TransactionPerRowResponse struct {
+type BankStatementPerRowResponse struct {
+	Index  int
 	Bank   string
 	Row    []string
 	Amount int
 }
 
 type Row struct {
-	RowKey          int    `json:"key"`
-	TrxID           string `json:"trx_id"`
-	Amount          int    `json:"amount"`
-	Type            string `json:"type"`
-	TransactionTime string `json:"transaction_time"`
+	TransactionRowIndex int    `json:"key"`
+	TrxID               string `json:"trx_id"`
+	Amount              int    `json:"amount"`
+	Type                string `json:"type"`
+	TransactionTime     string `json:"transaction_time"`
 }
 
 func main() {
@@ -65,10 +77,12 @@ func reconcile(w http.ResponseWriter, req *http.Request) {
 	_ = bankStatementsResult
 	var validTransactionsKey []int
 	var requestBody ReconcileRequest
-	var transactionsResult TransactionsReadResponse
-	transactionsResult.MapRows = make(map[int]bool)
-	unbufferedCh := make(chan TransactionPerRowResponse)
-	matchedBankTransaction := make(map[string][]Row)
+	transactionsResult := TransactionsReadResponse{
+		MapRows:              make(map[int]bool),
+		UnmatchedTransaction: make(map[int]Row),
+	}
+	unbufferedCh := make(chan BankStatementPerRowResponse)
+	unmatchedBankStatement := make(map[string]map[int]Row)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -150,19 +164,25 @@ func reconcile(w http.ResponseWriter, req *http.Request) {
 				rowAmount := absInt(result.Amount)
 				isValid := isTransactionValid(validTransactionsKey, amountVal)
 				if amountVal == rowAmount && transactionType == result.Row[2] && !isValid {
-
 					validTransactionsKey = append(validTransactionsKey, k)
-					matchedBankTransaction[result.Bank] = append(matchedBankTransaction[result.Bank], Row{
-						RowKey:          k,
-						TrxID:           result.Row[0],
-						Amount:          amountVal,
-						Type:            result.Row[1],
-						TransactionTime: result.Row[2],
-					})
-
-					delete(transactionsResult.UnmatchedTransaction, k+1)
+					delete(transactionsResult.UnmatchedTransaction, k)
+					delete(unmatchedBankStatement[result.Bank], k)
 					break
 				}
+
+				if _, ok := unmatchedBankStatement[result.Bank]; !ok {
+					unmatchedBankStatement[result.Bank] = make(map[int]Row)
+				}
+
+				// Append unmatched bank transaction
+				unmatchedBankStatement[result.Bank][result.Index] = Row{
+					TransactionRowIndex: k,
+					TrxID:               result.Row[0],
+					Amount:              amountVal,
+					Type:                result.Row[1],
+					TransactionTime:     result.Row[2],
+				}
+
 			}
 		}
 	}()
@@ -176,27 +196,57 @@ func reconcile(w http.ResponseWriter, req *http.Request) {
 	//	IsExistInArray()
 	//}
 
-	fmt.Println("Total Transaction Processed: ", transactionsResult.TransactionCounter)
-	fmt.Println("Total Matched Transaction: ", transactionsResult.TransactionCounter-len(transactionsResult.UnmatchedTransaction))
-	fmt.Println("Total Unmatched Transaction: ", len(transactionsResult.UnmatchedTransaction))
-	fmt.Println("List Of Missing Transactions in Bank Statements ")
+	var response ReconcileResponse
+
+	var missingTransactions []Row
 	for _, v := range transactionsResult.UnmatchedTransaction {
-		fmt.Println(v)
+		missingTransactions = append(missingTransactions, v)
 	}
 
-	fmt.Println("List Of Missing Bank Statements In Transactions ")
-	//for _, itemMatchedBankTransaction := range matchedBankTransaction {
+	missingBankStatements := make(map[string][]Row)
+	for _, v := range requestBody.BankStatementsPath {
+		for _, x := range unmatchedBankStatement[v] {
+			missingBankStatements[v] = append(missingBankStatements[v], x)
+		}
+	}
+
+	response.TotalTransactionsProceed = transactionsResult.TransactionCounter
+	response.TotalMatchedTransactions = transactionsResult.TransactionCounter - len(transactionsResult.UnmatchedTransaction)
+	response.TotalUnmatchedTransactions = TotalUnmatchedTransactionsData{
+		MissingTransactions:   missingTransactions,
+		MissingBankStatements: missingBankStatements,
+	}
+
+	//fmt.Println("Total Transaction Processed: ", transactionsResult.TransactionCounter)
+	//fmt.Println("Total Matched Transaction: ", transactionsResult.TransactionCounter-len(transactionsResult.UnmatchedTransaction))
+	//fmt.Println("Total Unmatched Transaction: ", len(transactionsResult.UnmatchedTransaction))
+	//fmt.Println("List Of Missing Transactions in Bank Statements ")
+	//for _, v := range transactionsResult.UnmatchedTransaction {
+	//	fmt.Println(v)
+	//}
+	//
+	//fmt.Println("List Of Missing Bank Statements In Transactions ")
+	//for k, itemMatchedBankTransaction := range unmatchedBankStatement {
+	//	fmt.Println("Bank Path: ", k)
 	//	for _, v := range itemMatchedBankTransaction {
-	//		transactionsResult.
+	//
 	//	}
 	//}
 
-	fmt.Println("HERE")
+	payload, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Failed marshall", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(payload)
 }
 
-func readBankStatementsV2(ctx context.Context, ch chan ChannelResponse, unbufferedCh chan TransactionPerRowResponse, wg *sync.WaitGroup, path string, startDate, endDate time.Time) {
+func readBankStatementsV2(ctx context.Context, ch chan ChannelResponse, unbufferedCh chan BankStatementPerRowResponse, wg *sync.WaitGroup, path string, startDate, endDate time.Time) {
 	defer wg.Done()
 	response := ChannelResponse{}
+	var transactionRowIndex int
 
 	for {
 		select {
@@ -248,12 +298,13 @@ func readBankStatementsV2(ctx context.Context, ch chan ChannelResponse, unbuffer
 				response.result = append(response.result, record)
 
 				fmt.Println("Send Amount:", response.amount, " From Path: ", path)
-				unbufferedCh <- TransactionPerRowResponse{
+				unbufferedCh <- BankStatementPerRowResponse{
+					Index:  transactionRowIndex,
 					Row:    record,
 					Amount: amountVal,
 					Bank:   path,
 				}
-
+				transactionRowIndex++
 			}
 
 			fmt.Println("Final Result Path: ", path, " Amount: ", response.amount)
@@ -264,8 +315,10 @@ func readBankStatementsV2(ctx context.Context, ch chan ChannelResponse, unbuffer
 }
 
 func readTransactions(path string, startDate, endDate time.Time) (TransactionsReadResponse, error) {
-	var response TransactionsReadResponse
-	response.UnmatchedTransaction = make(map[int]Row)
+	response := TransactionsReadResponse{
+		UnmatchedTransaction: make(map[int]Row),
+		MapRows:              make(map[int]bool),
+	}
 
 	transactionFile, err := os.Open(path)
 	if err != nil {
@@ -313,11 +366,11 @@ func readTransactions(path string, startDate, endDate time.Time) (TransactionsRe
 
 		response.Rows = append(response.Rows, record)
 		response.UnmatchedTransaction[response.TransactionCounter] = Row{
-			RowKey:          response.TransactionCounter,
-			TrxID:           record[0],
-			Amount:          amountVal,
-			Type:            record[1],
-			TransactionTime: record[2],
+			TransactionRowIndex: response.TransactionCounter,
+			TrxID:               record[0],
+			Amount:              amountVal,
+			Type:                record[1],
+			TransactionTime:     record[2],
 		}
 		response.MapRows[response.TransactionCounter] = true
 
